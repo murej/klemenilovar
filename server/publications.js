b776ecaf9269314e66d705e08f86fa4bacdeeb7e
Jure Martinec <jure.martinec@gmail.com>
2015-11-15 22:13:15 +0100

    Manage Collections

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,10 @@ Meteor.publish("projects", function () {
 Meteor.publish("items", function () {
   return Items.find();
 });
+Meteor.publish("collections", function () {
+  return Collections.find();
+});
+
 
 Houston.methods(Pages, {
   "Publish": function (item) {
@@ -35,5 +39,103 @@ Houston.methods(Pages, {
     Pages.update(item._id, {$set: {
       published: false
     }});
+  }
+});
+
+Houston.methods(Projects, {
+  "Publish": function (item) {
+
+    var slug = slugify(item.name);
+    var createdAt = moment(item.createdAt).isValid() ? item.createdAt : new Date();
+
+    var query = Projects.find({ slug: slug, published: true });
+    var slugExists =
+      (query.count() !== 0) &&
+      !_.some(query.fetch(), function(el){ return el._id === item._id });
+
+    if(slugExists) {
+      throw new Error("Project with that name already exists. Please choose another one.");
+    }
+    else {
+      Projects.update(item._id, {$set: {
+        createdAt: createdAt,
+        slug: slug,
+        published: true
+      }});
+
+      var collectionNames = item.collections.split(", ").forEach(function(collectionName) {
+        var slug = slugify(collectionName);
+        var collectionExists = Collections.find({slug: slug}).count() !== 0;
+        if(!collectionExists) {
+          Collections.insert({
+            name: collectionName,
+            slug: slug
+          });
+        }
+      });
+
+      return item.name + " project has been published.";
+    }
+  },
+  "Unpublish": function (item) {
+    Projects.update(item._id, {$set: {
+      published: false
+    }});
+
+    Meteor.call("checkCollectionPopulation", item);
+
+    return item.name + " project unpublished.";
+  }
+});
+
+Houston.methods(Items, {
+  "Publish": function (item) {
+    var createdAt = moment(item.createdAt).isValid() ? item.createdAt : new Date();
+
+    Items.update(item._id, {$set: {
+      createdAt: createdAt,
+      published: true
+    }});
+
+    var collections = item.collections && item.collections.split(", ");
+    var hasCollections = !_.isEmpty(collections);
+    var collectionNames = hasCollections && collections.forEach(function(collectionName) {
+      var slug = slugify(collectionName);
+      var collectionExists = Collections.find({slug: slug}).count() !== 0;
+      if(!collectionExists) {
+        Collections.insert({
+          name: collectionName,
+          slug: slug
+        });
+      }
+    });
+
+    return "Item has been published.";
+  },
+  "Unpublish": function (item) {
+    Items.update(item._id, {$set: {
+      published: false
+    }});
+
+    Meteor.call("checkCollectionPopulation", item);
+
+    return "Item unpublished.";
+  }
+});
+
+Meteor.methods({
+  checkCollectionPopulation(item) {
+    var collections = item.collections && item.collections.split(", ");
+    var hasCollections = !_.isEmpty(collections);
+    var collectionNames = hasCollections && collections.forEach(function(collectionName) {
+      var slug = slugify(collectionName);
+      var hasProjects = Projects.find({published: true, collections: collectionName}).count() !== 0;
+      var hasItems = Items.find({published: true, collections: collectionName}).count() !== 0;
+      if(!hasProjects && !hasItems) {
+        Collections.remove({
+          slug: slug
+        });
+      }
+    });
   }
 });
