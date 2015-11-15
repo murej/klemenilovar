de31904a5f88fc2e788394cdd99872679277fc80
Jure Martinec <jure.martinec@gmail.com>
2015-11-15 23:32:13 +0100

    Make home page the Portfolio collection

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,32 +1,8 @@
 HomePage = React.createClass({
 
-  getInitialState() {
-    return {
-      // hideCompleted: false
-    }
-  },
-
-  // This mixin makes the getMeteorData method work
-  mixins: [ReactMeteorData],
-
-  // Loads items from the Tasks collection and puts them on this.data.tasks
-  getMeteorData() {
-    Meteor.subscribe("pages");
-
-    let query = {
-
-    };
-
-    return {
-      page: Pages.find(query, {sort: {createdAt: -1}}).fetch(),
-    }
-  },
-
   render() {
     return (
-      <div className="HomePage">
-        Home Page
-      </div>
+      <CollectionPage params={{slug: "portfolio"}} />
     );
   }
 });
