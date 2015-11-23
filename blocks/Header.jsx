c34f61afd83ad4281f06909442c484b17c899b94
Jure Martinec <jure.martinec@gmail.com>
2015-11-23 01:51:56 +0100

    Update header

--- blocks/Header.jsx
+++ blocks/Header.jsx
@@ -1,4 +1,8 @@
-const {Link} = ReactRouter;
+const {
+  Router,
+  Route,
+  Link
+} = ReactRouter;
 
 Header = React.createClass({
 
@@ -10,15 +14,16 @@ Header = React.createClass({
 
     return {
       pages: Pages.find({ published: true }, {fields: {name: 1, slug: 1}}).fetch(),
-      collections: Collections.find({}).fetch()
+      collections: Collections.find({ published: true }, {fields: {name: 1, slug: 1}}).fetch()
     }
   },
 
   getPages() {
-    var pages = this.data.pages.map(function(page) {
+    var pages = !_.isEmpty(this.data.pages) && this.data.pages.map(function(page) {
+
       return (
-        <li key={page._id}>
-          <Link to={`/${page.slug}`}>{page.name}</Link>
+        <li key={page._id} className="Header-NavigationElement">
+          <Link to={`/${page.slug}`} activeStyle={{ textDecoration: "line-through" }}>{page.name}</Link>
         </li>
       )
     });
@@ -27,16 +32,42 @@ Header = React.createClass({
   },
 
   getCollections() {
-    var collections = this.data.collections.map(function(collection) {
+    var collections = !_.isEmpty(this.data.collections) && this.data.collections.map(function(collection) {
       return (
-        <li key={collection._id}>
-          <Link to={`/collection/${collection.slug}`}>{collection.name}</Link>
+        <li key={collection._id} className="Header-NavigationElement">
+          <Link to={`/collection/${collection.slug}`} activeStyle={{ textDecoration: "line-through" }}>{collection.name}</Link>
         </li>
       );
     });
 
     return collections;
   },
+
+  // generateLogo() {
+  //   var letters = ["k","l","e","m","e","n","i","l","o","v","a","r"];
+  //   var logo = letters.map(function(letter, index) {
+  //
+  //     var angle = "0deg";
+  //     var breakTag;
+  //
+  //     if(index > 0 && index < 7) {
+  //       angle = (15 * (index+1)) + "deg";
+  //     }
+  //     else if(index >= 7 && index < 12 ) {
+  //       angle = (90 - (15 * (index+1))) + "deg";
+  //       breakTag = <br />;
+  //     }
+  //
+  //     var style = {
+  //       webkitTransform: "rotate("+angle+")",
+  //       mozTransform: "rotate("+angle+")",
+  //       transform: "rotate("+angle+")"
+  //     }
+  //     return <span key={index} style={style}>{breakTag}{letter}</span>;
+  //   });
+  //
+  //   return logo;
+  // },
 
   render() {
     var loaded = !_.isEmpty(this.data.pages);
@@ -47,10 +78,26 @@ Header = React.createClass({
 
     return (
       <div className="Header">
-        <ul>
+        <ul className="Header-Navigation">
           {this.getPages()}
           {this.getCollections()}
         </ul>
+        <div className="Header-Logo">
+          <Link to="/">
+            <span className="Header-Logo-1">k</span>
+            <span className="Header-Logo-2">l</span>
+            <span className="Header-Logo-3">e</span>
+            <span className="Header-Logo-4">m</span>
+            <span className="Header-Logo-5">e</span>
+            <span className="Header-Logo-6">n</span>
+            <span className="Header-Logo-7">i</span><br />
+            <span className="Header-Logo-8 under">l</span><br />
+            <span className="Header-Logo-9 under">o</span><br />
+            <span className="Header-Logo-10 under">v</span><br />
+            <span className="Header-Logo-11 under">a</span><br />
+            <span className="Header-Logo-12 under">r</span>
+          </Link>
+        </div>
       </div>
     );
   }
